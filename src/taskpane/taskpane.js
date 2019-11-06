7d46fd34590b726771022b2410f98ba78a24121f
ChandanaRDeshmukh96 <chandana.r@triconinfotech.com>
2019-11-06 18:37:35 +0530

    chart write

--- src/taskpane/taskpane.js
+++ src/taskpane/taskpane.js
@@ -18,6 +18,8 @@ Office.onReady(info => {
     // Assign event handlers and other initialization logic.
     document.getElementById("create-table").onclick = createTable; // create table
     document.getElementById("color-cells").onclick = colorCells; // make cells  yellow
+    document.getElementById("create-line-chart").onclick = lineChart;
+    document.getElementById("compare-charts").onclick = compareCharts;
   }
 });
 
@@ -43,15 +45,11 @@ export async function colorCells() {
   }
 }
 
-function createTable() {
+function createTable(tableName, colNames, tableData, numCol, startCoord, endCoord) {
   Excel.run(function(context) {
     var currentWorksheet = context.workbook.worksheets.getActiveWorksheet();
-    var expensesTable = currentWorksheet.tables.add("A3:D3", true /*hasHeaders*/);
-    expensesTable.name = "ExpensesTable";
-
-    expensesTable.getHeaderRowRange().values = [["Date", "Merchant", "Category", "Amount"]];
-
-    expensesTable.rows.add(null /*add at the end*/, [
+    var expensesTable = currentWorksheet.tables.add(startCoord+":"+endCoord, true /*hasHeaders*/);
+    var data = tableData?tableData:[
       ["1/1/2017", "The Phone Company", "Communications", "120"],
       ["1/2/2017", "Northwind Electric Cars", "Transportation", "142.33"],
       ["1/5/2017", "Best For You Organics Company", "Groceries", "27.9"],
@@ -59,9 +57,14 @@ function createTable() {
       ["1/11/2017", "Bellows College", "Education", "350.1"],
       ["1/15/2017", "Trey Research", "Other", "135"],
       ["1/15/2017", "Best For You Organics Company", "Groceries", "97.88"]
-    ]);
+    ];
+    expensesTable.name = tableName? tableName:"ExpensesTable";
 
-    expensesTable.columns.getItemAt(3).getRange().numberFormat = [["€#,##0.00"]];
+    expensesTable.getHeaderRowRange().values = colNames?colNames:[["Date", "Merchant", "Category", "Amount"]];
+
+    expensesTable.rows.add(null /*add at the end*/, data);
+
+    expensesTable.columns.getItemAt(numCol?numCol:3).getRange().numberFormat = [["€#,##0.00"]];
     expensesTable.getRange().format.autofitColumns();
     expensesTable.getRange().format.autofitRows();
 
@@ -72,4 +75,81 @@ function createTable() {
       console.log("Debug info: " + JSON.stringify(error.debugInfo));
     }
   });
+}
+
+async function lineChart(){
+
+    var tableName = "ChartTable";
+    var colNames = [['Item','Cost']];
+    var tableData = [['milk','18'],['sugar','20'],['tea powder','25'],['ginger','40']];
+    var numCol = 1;
+    var startCoord = 'A10';
+    var endCoord = 'B10';
+    createTable(tableName, colNames, tableData, numCol, startCoord, endCoord);
+    try{
+      await Excel.run(async context => {
+        var sheet = context.workbook.worksheets.getItem('Sheet1');
+        var dataRange = sheet.getRange("A10:B13");
+        var chart = sheet.charts.add("Line", dataRange, "auto");
+        console.log(sheet.charts);
+        chart.title.text = "Sales Data";
+        chart.legend.position = "right"
+        chart.legend.format.fill.setSolidColor("white");
+        chart.dataLabels.format.font.size = 15;
+        chart.dataLabels.format.font.color = "black";
+        await context.sync();
+      }).catch(function(error) {
+        console.log("Error: " + error);
+        if (error instanceof OfficeExtension.Error) {
+          console.log("Debug info: " + JSON.stringify(error.debugInfo));
+        }
+      });
+    }catch(err){
+      console.log(err);
+    }
+    
+}
+
+async function compareCharts(){
+
+    /**
+     * Point to the chart
+     * compare the properties of the chart
+     * if properties are same, chart can be declared similar
+     */
+
+     Excel.run( context =>{
+      var sheet = context.workbook.worksheets.getItem("Sheet1" /* sheet name */); 
+      /* number of  charts in the sheet */
+      var chartCount  = sheet.charts.getCount(); 
+      var firstChart = sheet.charts.getItemAt(0 /* index of chart, starting from 0 */);
+      var lastChart = sheet.charts.getItemAt(1);
+
+      /** loading all properties that make a difference in the chart */
+
+      firstChart.load(`axes, chartType, dataLabels, format, left, legend, name, pivotOptions, 
+      plotBy, series, style, title`);
+      firstChart.axes.categoryAxis.title.load('text');
+      // console.log(Excel.ChartAxes.getItem('Category', 'Primary'));
+      lastChart.load(`axes, chartType, dataLabels, format, left, legend, name, pivotOptions, 
+      plotBy, series, style, title`);
+      lastChart.series.load('XValues');
+      
+      return context.sync()
+      .then(function () {
+      console.log(chartCount.value);
+
+      /** toJSON returns an object containing the properties that were loaded before */
+      console.log(firstChart);
+      console.log(firstChart.toJSON()); 
+      console.log(lastChart.toJSON());
+
+      firstChart.axes.categoryAxis.title.text = 'item';
+      });
+    }).catch(e => errorHandlerFunction(e))
+}
+
+function errorHandlerFunction(e){
+  console.log("error occured");
+  console.log(e);
 }
